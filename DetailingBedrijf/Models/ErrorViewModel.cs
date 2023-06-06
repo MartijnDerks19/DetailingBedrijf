9416c3ff6fe40dc2f10f8d0c6677efd738fc8189
Martijn Derks <martijnderks2@gmail.com>
2023-06-06 10:20:35 +0200

    Add project files.

---
+++ DetailingBedrijf/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DetailingBedrijf.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
