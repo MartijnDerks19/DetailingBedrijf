8d5af7347825274c173d749ac6dbafb984d4695a
Martijn Derks <martijnderks2@gmail.com>
2023-06-06 15:29:47 +0200

    Push before break

--- DetailingBedrijf/Controllers/DashboardController.cs
+++ DetailingBedrijf/Controllers/DashboardController.cs
@@ -2,6 +2,8 @@
 
 namespace DetailingBedrijf.Controllers
 {
+    //ToDo: Aanmaken van een dashboard. Lijst van detailers word getoond. Op het moment dat er op details wordt geklikt wordt een agenda getoond. 
+    //ToDo: In dit dashboard kunnen afspraken worden aangemaakt, verwijderd en ingezien.
     public class DashboardController : Controller
     {
         public IActionResult Index()
