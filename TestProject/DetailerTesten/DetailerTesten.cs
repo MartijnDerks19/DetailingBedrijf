9409bdeac1e90740ad19a5e831b647c81e4e17db
Martijn Derks <martijnderks2@gmail.com>
2023-06-07 13:18:33 +0200

    Verandering aan testen

--- TestProject/DetailerTesten/DetailerTesten.cs
+++ TestProject/DetailerTesten/DetailerTesten.cs
@@ -91,22 +91,5 @@ namespace TestProject.DetailerTesten
             //Assert
             Assert.Equal(expectedExceptionMessage, actualErrorMessage.Message);
         }
-
-        public List<DetailerDTO> VulLijst(int hoeveelheid)
-        {
-            int id = 0;
-            List<DetailerDTO> detailers = new List<DetailerDTO>();
-            for (int i = 0; i < hoeveelheid; i++)
-            {
-                DetailerDTO dto = new DetailerDTO()
-                {
-                    DetailerID = id,
-                    Naam = "Test"
-                };
-                detailers.Add(dto);
-                id++;
-            }
-            return detailers;
-        }
     }
 }
