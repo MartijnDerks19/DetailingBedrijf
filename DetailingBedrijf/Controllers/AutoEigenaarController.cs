c7614239da7e97a83fa4a806b831258c35d1683f
Martijn Derks <martijnderks2@gmail.com>
2023-06-16 11:44:07 +0200

    Commit

--- DetailingBedrijf/Controllers/AutoEigenaarController.cs
+++ DetailingBedrijf/Controllers/AutoEigenaarController.cs
@@ -9,19 +9,19 @@ namespace DetailingBedrijf.Controllers
 {
     public class AutoEigenaarController : Controller
     {
-        private AutoEigenaarCollectie _collectie;
+        private AutoEigenaarLogica _logica;
         private AutoEigenaarMapping _mapping = new AutoEigenaarMapping();
 
         public AutoEigenaarController(IConfiguration configuration)
         {
             IAutoEigenaar _autoEigenaarData = new AutoEigenaarDataToegang(configuration);
             IAuto _autoData = new AutoDataToegang(configuration);
-            AutoEigenaarCollectie collectie = new AutoEigenaarCollectie(_autoEigenaarData, _autoData);
-            _collectie = collectie;
+            AutoEigenaarLogica logica = new AutoEigenaarLogica(_autoEigenaarData, _autoData);
+            _logica = logica;
         }
         public IActionResult Index()
         {
-            List<AutoEigenaarModel> models = _collectie.HaalAllesOp();
+            List<AutoEigenaarModel> models = _logica.HaalAllesOp();
             return View(models);
         }
 
@@ -35,7 +35,7 @@ namespace DetailingBedrijf.Controllers
         [HttpPost]
         public IActionResult Aanmaken(AutoEigenaarModel model)
         {
-            _collectie.Aanmaken(_mapping.MapModelNaarDTO(model));
+            _logica.Aanmaken(_mapping.MapModelNaarDTO(model));
             return RedirectToAction("Index", "Dashboard");
         }
 
@@ -43,19 +43,25 @@ namespace DetailingBedrijf.Controllers
         [Route("AutoEigenaar/Details/{id}")]
         public IActionResult Details(int id)
         {
-            AutoEigenaarModel eigenaar = _collectie.OphalenOpID(id);
-            List<AutoModel> autos = _collectie.HaalAutosOpVoorEigenaar(id);
-            eigenaar.AutosEigenaar = autos;
-            return View(eigenaar);
+            AutoEigenaarModel model = _logica.OphalenOpID(id);
+            return View(model);
         }
 
         [Route("AutoEigenaar/Verwijderen/{id}")]
         public IActionResult Verwijderen(int id)
         {
-            _collectie.VerwijderenOpID(id);
+            _logica.VerwijderenOpID(id);
             return RedirectToAction("Index", "Dashboard");
         }
-
+        
+        [HttpGet]
+        [Route("Auto/AutosVanEigenaarOpID/{eigenaarID:int}")]
+        public IActionResult AutosVanEigenaarOpID(int eigenaarID)
+        {
+            AutoEigenaarModel eigenaarEnAutos = _logica.HaalAutosOpVoorEigenaar(eigenaarID);
+            return View(eigenaarEnAutos);
+        }
+        
         private string HaalConnectionStringOp(IConfiguration configuration)
         {
             var connectionString = configuration.GetConnectionString("ConnectionString");
